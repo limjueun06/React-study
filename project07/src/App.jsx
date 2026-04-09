aff24ec94dee94a211ec4364f77ad2a802848cf2
je <limjueun06@gmail.com>
2026-04-09 19:25:35 +0900

    20260409리액트시험복습

--- project07/src/App.jsx
+++ project07/src/App.jsx
@@ -4,23 +4,38 @@ import { useState } from 'react'
 
 function App() {
   const [inputValue, setInputValue] = useState('')
-  const [review, setReview] = useState('')
-  
+  const [review, setReview] = useState([])
+
   const clickButton = () => {
-    setReview(inputValue)
+    if (inputValue.trim() !== '') {
+      setReview([...review, inputValue])
+      setInputValue('')
+    }
+  }
+
+  const deleteTodo = (index) => {
+    const newReview = review.filter((_, i) => i !== index)
+    setReview(newReview)
   }
 
   return (
     <>
       <h2>Todo List</h2>
+
       <p>
-        <input value={inputValue} onChange={(e)=>setInputValue(e.target.value)} placeholder='할 일을 입력하세요'></input>
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="할 일을 입력하세요"
+        />
         <button onClick={clickButton}>추가</button>
       </p>
-    
-      <br></br>
-      
-     
+
+      <br />
+
+      {review.map((item, index) => (
+        <TodoBox key={index} todo={item} onDelete={()=>deleteTodo(index)}/>
+      ))}
     </>
   )
 }
--- project07/src/components/TodoBox.jsx
+++ project07/src/components/TodoBox.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const TodoBox = () => {
+const TodoBox = ({ todo, onDelete}) => {
   return (
     <div>
-        <div style={{border:'3px double gray', padding: '3%', margin: '3%'}}>
-            <p>{review}</p>
-        </div>
+      <div style={{ border: '3px double gray', padding: '3%'}}>
+        <input type="checkbox"></input>
+        <span>{todo}</span>
+        <button onClick={onDelete}>삭제</button>
+      </div>
     </div>
   )
 }
